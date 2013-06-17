a0d9923715195c5c494c7bd9d9131dbecf970661
Luc Perkins <luc@Lucs-MacBook-Pro.local>
2013-06-17 15:26:44 -0700

    small typo fix

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 app.jinja_env.add_extension('pyjade.ext.jinja.PyJadeExtension')
 
-janrain_engage_root_url = ‘https://rpxnow.com/api/v2/auth_info”
+janrain_engage_root_url = "https://rpxnow.com/api/v2/auth_info”
 
 @app.route('/')
 def index():
